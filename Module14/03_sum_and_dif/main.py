c343ac078b6e56250fdb8d2da9ce48228c52ced8
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-25 09:00:37 +0400

    .

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def first_fun(number):
+    summ = 0
+    while number != 0:
+        one_num = number % 10
+        number //= 10
+        summ += one_num
+    return summ
+
+def second_fun(number):
+    count = 0
+    while number != 0:
+        one_num = number % 10
+        number //= 10
+        count += 1
+    return count
+
+
+number = int(input('Введите число: '))
+
+print('Сумма чисел:', first_fun(number))
+print('Количество цифр в числе:', second_fun(number))
+print('Разность суммы и количества цифр:', first_fun(number) - second_fun(number))
