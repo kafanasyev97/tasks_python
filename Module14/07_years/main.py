988f2323f96e2b8401e15e3e873505849e4aef1c
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-25 11:02:56 +0400

    .

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+first_year = int(input('Введите первый год: '))
+second_year = int(input('Введите первый год: '))
+for total_year in range(first_year, second_year + 1):
+    count = 0
+    for number in str(total_year):
+      result = str(total_year % 10)
+      if number == result:
+          count += 1
+          if count == 3:
+              print(total_year)
