1157e7605f4c0d47e7d8d3a1a7e58f21139e0c48
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-25 10:18:43 +0400

    .

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+user = int(input('Введите число: '))
+for result in range(user, 1, -1):
+    if user % result == 0:
+        number = result
+print('Наименьший делитель, отличный от единицы:', number)
