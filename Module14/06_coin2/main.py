51d51a943e1f6d06521e6f7d7a10c9cd6f9c1177
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-26 07:38:34 +0400

    .

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -2,7 +2,7 @@ print('Введите координаты монетки:')
 x = float(input('x: '))
 y = float(input('y: '))
 r = float(input('Введите радиус: '))
-if x <= r and y <= r:
+if (x ** 2 + y ** 2) ** 0.5 <= r:
     print('Монетка где то рядом')
 else:
     print('Монетки в области нет')
