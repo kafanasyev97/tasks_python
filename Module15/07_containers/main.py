2f3992539f39bac2d69487662db17b220dcf735b
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-27 11:12:47 +0400

    .

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+total_containers = int(input('Количество контейнеров: '))
+containers_list = []
+
+for i in range(total_containers):
+    one_container = int(input('Введите вес контейнера: '))
+    if one_container > 200:
+        while one_container > 200:
+            one_container = int(input('Ошибка! Вес контейнера не должен превышать 200. Введите вес еще раз: '))
+    containers_list.append(one_container)
+
+user_weight = int(input('Введите вес нового контейнера: '))
+count = 1
+
+for i in containers_list:
+    if user_weight > i:
+        break
+    elif user_weight <= i:
+        count += 1
+
+print('Номер, который получит новый контейнер:', count)
--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+n = int(input('Введите количество чисел: '))
+first_list = []
+second_list = []
+
+for i in range(n):
+    user = int(input('Введите число: '))
+    first_list.append(user)
+
+k = int(input('Сдвиг: '))
+count = k
+while count != 0:
+    second_list.append(first_list[len(first_list) - count])
+    count -= 1
+
+numbers = n - k  #оставшееся кол-во цифр
+for i in range(numbers):
+    second_list.append(first_list[count])
+    count += 1
+
+print('Изначальный список:', first_list)
+print('Сдвинутый список:', second_list)
