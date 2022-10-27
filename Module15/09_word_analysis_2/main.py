cec7356459fccaf62d61118fc01715768b58cf70
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-27 11:36:44 +0400

    .

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+user = input('Введите слово: ')
+start_list = list(user)
+finish_list = []
+count = 1
+
+for i in range(len(start_list)):
+    finish_list.append(start_list[len(start_list) - count])
+    count += 1
+
+if start_list == finish_list:
+    print('Слово является палиндромом')
+else:
+    print('Слово не является палиндромом')
