816cee3ba3c3db93001ad654ba120328c54b78b4
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-27 08:17:24 +0400

    .

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,17 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+user_count = int(input('Сколько фильмов хотите добавить? '))
+user_list = []
 
-# TODO здесь писать код
+for _ in range(user_count):
+    count = 0
+    choice = input('Введите название фильма: ')
+    for text in films:
+        if text == choice:
+            user_list.append(choice)
+            count += 1
+    if count == 0:
+        print('Ошибка: фильма', choice, 'у нас нет :(')
+
+print('Ваш список любимых фильмов:', user_list)
--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+user = input('Введите слово: ')
+text_list = list(user)
+need_number = 0
+
+for i in text_list:
+    count = 0
+    for sym in text_list:
+        if sym == i:
+            count += 1
+    if count == 1:
+        need_number += 1
+
+print('Количество уникальных букв:', need_number)
