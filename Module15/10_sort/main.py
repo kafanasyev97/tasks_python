b2400a2ccc84d6041d5ed6120211f823f9db906f
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-27 12:54:15 +0400

    .

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+total_num = int(input('Введите количество чисел: '))
+n = 0
+num_list = []
+for _ in range(total_num):
+    user = int(input('Введите число: '))
+    num_list.append(user)
+
+for b in range(total_num - 1):
+    n = 0
+    for i in range(total_num - 1):
+        if num_list[n] > num_list[n + 1]:
+            num_list[n], num_list[n + 1] = num_list[n + 1], num_list[n]
+        n += 1
+
+print(num_list)
