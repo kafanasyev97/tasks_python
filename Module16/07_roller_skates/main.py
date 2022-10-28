2b2bd1d9ad897e4b0f1de208f4edabc82b9b20b9
Kirill Afanasyev <kirillmas222@gmail.com>
2022-10-29 02:25:29 +0400

    .

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+skates = int(input('Кол-во коньков: '))
+skates_list = []
+for i_size in range(skates):
+    size_skates = int(input(f'Размер {i_size + 1}-й пары: '))
+    skates_list.append(size_skates)
+
+people = int(input('Кол-во людей: '))
+people_list = []
+for i_people in range(people):
+    size_people = int(input(f'Размер ноги {i_people + 1}-ого человека: '))
+    people_list.append(size_people)
+
+count = 0
+
+for i in people_list:
+    for n in skates_list:
+        if i <= n:
+            count += 1
+            skates_list.remove(n)
+            break
+print('Наибольшее кол-во людей, которые могут взять ролики:', count)
--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+def need_func(num_list):
+    reverse_list = []
+    for i_num in range(len(num_list) - 1, -1, -1):
+        reverse_list.append(num_list[i_num])
+    if num_list == reverse_list:
+        return True
+    else:
+        return False
+
+count_nums = int(input('Кол-во чисел: '))
+nums_list = []
+
+for _ in range(count_nums):
+    user = int(input('Число: '))
+    nums_list.append(user)
+
+new_nums = []
+result = []
+
+for i_nums in range(0, count_nums):
+    for i_help in range(i_nums, count_nums):
+        new_nums.append(nums_list[i_help])
+    if need_func(new_nums):
+        for i_result in range(0, i_nums):
+            result.append(nums_list[i_result])
+        result.reverse()
+        break
+    new_nums = []
+
+print('Последовательность:', nums_list)
+print('Нужно приписать чисел:', len(result))
+print('Сами числа:', result)
