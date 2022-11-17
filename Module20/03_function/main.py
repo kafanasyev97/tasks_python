8a6edaa8028fbd8f2a907a02677e96fa5f1a5f6f
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-18 01:35:05 +0400

    .

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def slicer(cort, number):
+    if number not in cort:
+        return {}
+    for x in cort:
+        if x == number:
+            b = cort[cort.index(x):]
+            break
+    for x in range(1, len(b)):
+        if b[x] == number:
+            b = b[:x + 1]
+            break
+    return tuple(b)
+
+print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
