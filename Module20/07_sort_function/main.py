57ebc625840516d2fe9effaa480d4cd7d19edb63
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-18 15:06:20 +0400

    Оставил примечание

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def tpl_sort(cort):
+    for x in cort:
+        if x != int(x):
+            return cort
+    corted = sorted(list(cort))
+    return tuple(corted)
+
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
