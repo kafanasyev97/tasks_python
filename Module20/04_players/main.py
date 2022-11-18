17dba9a9ff77def4beeb254c02851ee04c67c771
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-18 12:49:57 +0400

    Оставил примечание

--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -3,5 +3,12 @@ players = {
     ("Bob", "Robbin"): (7, 5, 14),
     ("Rob", "Bobbin"): (12, 8, 2)
 }
+# asd = []
+# for index, value in players.items():
+#     index, value = list(index), list(value)
+#     asd.append(tuple(index + value))
+# print(asd)
+# Сначала хотел сделать так, но потом узнал, что кортежи оказывается можно складывать между собой
 
-# TODO здесь писать код
+result = [index + value for index, value in players.items()]
+print(result)
