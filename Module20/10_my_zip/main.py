e864196e25cf34f1d0488cb1f52c564fea723fb3
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-19 14:19:13 +0400

    .

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+string = 'abcd'
+numbers = (10, 20, 30, 40)
+cort = ((string[i], numbers[i]) for i in range(min(len(string), len(numbers))))
+print(cort)
+for i_elem in cort:
+    print(i_elem)
