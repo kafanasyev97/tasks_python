c7a998a788ba3d4c15e1423885d34e893413116c
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-23 02:14:53 +0400

    .

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def need_func(a, b):
+    spis = [(a[i_elem], b[i_elem])
+             for i_elem in range(min(len(a), len(b)))]
+    return spis
+
+
+# syms_str = 'abcd'
+# nums_tpl = (10, 20, 30, [1, 33, 2])
+# print(need_func(syms_str, nums_tpl))
