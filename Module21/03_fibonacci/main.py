2c0ceafab94a29fd5a018cfb9f985992571c9979
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-23 03:04:47 +0400

    .

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def fibonacci(n):
+    cur = 1
+    if n > 2:
+        cur = fibonacci(n-1) + fibonacci(n-2)
+    return cur
+
+element = int(input('Введите позицию числа в ряде Фибоначчи: '))
+value = fibonacci(element)
+print('Число:', value)
