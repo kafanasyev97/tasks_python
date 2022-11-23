d1f530ca0887129ea36d6b2ffeef700edb5639b3
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-23 15:20:39 +0400

    .

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,18 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+def calculating_math_func(data, i_dict):
+    if data in i_dict:
+        return i_dict[data]
+    else:
+        result = max(i_dict.values())
+        for index in range(max(i_dict.keys()) + 1, data + 1):
+            result *= index
+            i_dict[index] = result
+        return result
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+slov = {1: 1}
+while True:
+    user = int(input('Введите число: '))
+    print(calculating_math_func(user, slov))
+
