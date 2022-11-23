cc80035eb5ee93318c44131be750edc759bc919c
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-24 00:07:07 +0400

    .

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,14 @@
+def need_func(spis, result=[]):
+    for num in spis:
+        if isinstance(num, int):
+            result.append(num)
+        else:
+            a = need_func(num)
+    return result
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print(need_func(nice_list))
+
+
