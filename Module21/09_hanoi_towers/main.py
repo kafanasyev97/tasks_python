029c364d542bda2154c86f3992d7ff66706612d3
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-24 02:21:51 +0400

    .

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def move(n, start, finish):
+    if n > 0:
+        tmp = 6 - start - finish
+        move(n - 1, start, tmp)
+        print(f'Переложить диск {n} со стержня номер {start} на стержень номер {finish}')
+        move(n - 1, tmp, finish)
+n=int(input('Введите количество дисков: '))
+move(n, 1, 3)
