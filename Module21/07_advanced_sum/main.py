93c01fecf03c4b6b7788ca68ef80784cab48a3d1
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-23 21:21:30 +0400

    .

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def sum_me(*args, summ=0):
+    for num in args:
+        if isinstance(num, int):
+            summ += num
+        else:
+            for x in num:
+                a = sum_me(x)
+                summ += a
+    return summ
+# print(sum_me([[1, 2, [3]], [1], 3]))
