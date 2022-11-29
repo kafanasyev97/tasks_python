ef36514ca0eb54c17246cc10312d08eaf114cb0c
Kirill Afanasyev <kirillmas222@gmail.com>
2022-11-30 00:27:15 +0400

    закоментированное решение сделал через функцию, но первое решение нравится больше

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+need_file = open('zen.txt', 'r')
+ssd = []
+for x in need_file:
+    ssd.append(x)
+need_file.close()
+ssd.reverse()
+for x in ssd:
+    if '\n' in x:
+        print(x)
+    else:
+        print(x, '\n')
+
+
+
+# def need_func(g, z):
+#     need_file = open(g, z)
+#     one_list = []
+#     for x in need_file:
+#         one_list.append(x)
+#     need_file.close()
+#     one_list.reverse()
+#     return one_list
+#
+#
+# a = 'zen.txt'
+# b = 'r'
+# ssd = need_func(a, b)
+# for x in ssd:
+#     if '\n' in x:
+#         print(x)
+#     else:
+#         print(x, '\n')
