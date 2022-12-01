982f1dd88b6f1365d73fa04ebd61a04554805e18
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-01 12:27:28 +0400

    Хотел узнать, можно ли это задание решить как нибудь короче? Может, какой нибудь другой алгоритм решения? Очень долго сидел с этой задачей, пришлось изучать лямбду раньше)

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+file = open('text.txt', 'r', encoding='utf-8')
+text = file.read()
+print('Содержимое файла text.txt:')
+print(text)
+file.close()
+
+need_text = ''
+need_dict = dict()
+for alpha in text:
+    if alpha.isalpha():
+        need_text += alpha
+
+need_text = need_text.lower()
+for i_elem in need_text:
+    need_dict[i_elem] = round(1 / (len(need_text) / need_text.count(i_elem)), 3)
+
+two_dict = {}
+for x in sorted(need_dict.keys()):
+    two_dict[x] = str(need_dict[x])
+
+ssd = []
+for a, b in two_dict.items():
+    ssd.append([a, b])
+ssd.reverse()
+
+ssd.sort(key=lambda num: num[1])
+ssd.reverse()
+
+result = ''
+for i_elem in ssd:
+    result += ' '.join(i_elem) + '\n'
+
+finish_file = open('analysis.txt', 'w')
+finish_file.write(result)
+print('\nСодержимое файла analysis.txt:')
+print(result)
+finish_file.close()
