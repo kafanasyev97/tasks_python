d62d708a25d92adf0b5809db2e7f955c9008cfcd
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-03 13:30:26 +0400

    .

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+summ = 0
+with open('calc.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        line = line.split()
+        try:
+            summ += eval(' '.join(line))
+        except Exception:
+            print('Обнаружена ошибка в строке:', ' '.join(line), end='')
+            answer = input('   Хотите исправить? ').lower()
+            if answer == 'да':
+                new_string = input('Введите исправленную строку: ').split()
+                summ += eval(' '.join(new_string))
+print('Сумма результатов:', summ)
