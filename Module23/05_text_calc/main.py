fbc361e7c69fe0b7648bee154e5dd6da26a8fcfe
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-04 12:02:53 +0400

    Исправил

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,13 +1,27 @@
+def need_func(elem, result):
+    if elem[1] == '+':
+        result += (int(elem[0]) + int(elem[2]))
+    elif elem[1] == '-':
+        result += (int(elem[0]) - int(elem[2]))
+    elif elem[1] == '*':
+        result += (int(elem[0]) * int(elem[2]))
+    elif elem[1] == '/':
+        result += (int(elem[0]) / int(elem[2]))
+    else:
+        raise Exception
+    return result
+
+
 summ = 0
 with open('calc.txt', 'r', encoding='utf-8') as file:
     for line in file:
         line = line.split()
         try:
-            summ += eval(' '.join(line))
+            summ = need_func(line, summ)
         except Exception:
             print('Обнаружена ошибка в строке:', ' '.join(line), end='')
             answer = input('   Хотите исправить? ').lower()
             if answer == 'да':
                 new_string = input('Введите исправленную строку: ').split()
-                summ += eval(' '.join(new_string))
+                summ = need_func(new_string, summ)
 print('Сумма результатов:', summ)
