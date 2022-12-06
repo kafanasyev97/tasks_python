1bd9937c3c717f1b8ac0384aba950f1d1e814f3a
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-07 01:13:20 +0400

    .

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+class Student:
+    def __init__(self, name, number_group, progress):
+        self.name = name
+        self.number_group = number_group
+        self.progress = progress
+        self.average = sum(progress) / len(progress)
+
+    def sort_by_list(self):
+        return self.average
+
+
+ssd = []
+for x in range(2):
+    name_user = input('Введите инициалы пользователя: ')
+    number_group_user = int(input('Введите номер группы: '))
+    grade_user = list(map(int, input('Введите оценки(через пробел): ').split()))
+    ssd.append(Student(name_user, number_group_user, grade_user))
+
+ssd.sort(key=Student.sort_by_list)
+
+for x in ssd:
+    print(x.name, x.number_group, '  ', *x.progress)
+
