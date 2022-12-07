1c1b6c77567ac2d9c4e9c3ae13083aaf13857f6b
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-07 11:27:58 +0400

    .

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import math
+
+
+class Circle:
+    def __init__(self, x=0, y=0, r=1):
+        self.x = x
+        self.y = y
+        self.r = r
+
+    def square(self):
+        return math.pi * self.r ** 2
+
+    def length(self):
+        return 2 * self.r * math.pi
+
+    def increase(self, k):
+        return self.length() * k
+
+    def intersection(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y) <= self.r + other.r
+
+
