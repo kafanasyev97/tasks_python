ad1f5c5dfa457f2e148410080f6321ef11c5459b
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-07 20:28:13 +0400

    .

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,98 @@
-# TODO здесь писать код
+class Fire:
+    title = 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Lava()
+        elif isinstance(other, Brick):
+            return Sand()
+        else:
+            return None
+
+
+class Air:
+    title = 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Shtorm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Dust()
+        else:
+            return None
+
+
+class Water:
+    title = 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Shtorm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+        elif isinstance(other, Brick):
+            return Clay()
+        else:
+            return None
+
+
+class Earth:
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Air):
+            return Dust()
+        else:
+            return None
+
+
+class Shtorm:
+    title = 'Шторм'
+
+
+class Steam:
+    title = 'Пар'
+
+
+class Dirt:
+    title = 'Грязь'
+
+
+class Lightning:
+    title = 'Молния'
+
+
+class Dust:
+    title = 'Пыль'
+
+
+class Lava:
+    title = 'Лава'
+
+
+class Brick:
+    title = 'Кирпич'
+
+
+class Sand:
+    title = 'Песок'
+
+
+class Clay:
+    title = 'Глина'
+
+
+f = Water()
+s = Brick()
+a = f + s
+print(a.title)
