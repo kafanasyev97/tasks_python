ad33316ee8c883b4caa37493fa7c423a67f48cfe
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-09 18:51:21 +0400

    .

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -2,21 +2,33 @@ import random
 
 
 class Warrior:
-    health = 100   #через __init__ не стал делать потому, что здоровье у обоих одинаковое
+    def __init__(self, health):
+        self.health = health
+
+    def hit(self):
+        self.health -= 20
+
+    def live(self):
+        if self.health > 0:
+            print('Жив!\n')
+        else:
+            print('Погиб!\n')
 
 
-warrior_1 = Warrior()
-warrior_2 = Warrior()
+warrior_1 = Warrior(100)
+warrior_2 = Warrior(100)
 while True:
     answer = random.randint(1, 2)
     if answer == 1:
-        warrior_2.health -= 20
+        warrior_2.hit()
         print('Атаковал 1 воин')
-        print('Здоровье 2 воина:', warrior_2.health, '\n')
+        print('Здоровье 2 воина:', warrior_2.health)
+        warrior_2.live()
     elif answer == 2:
-        warrior_1.health -= 20
+        warrior_1.hit()
         print('Атаковал 2 воин')
-        print('Здоровье 1 воина:', warrior_1.health, '\n')
+        print('Здоровье 1 воина:', warrior_1.health)
+        warrior_1.live()
 
     if warrior_1.health == 0:
         print('Победу одержал 2 воин!')
@@ -24,3 +36,47 @@ while True:
     elif warrior_2.health == 0:
         print('Победу одержал 1 воин!')
         break
+
+
+
+# import random
+#
+#
+# class Warrior:
+#     def __init__(self, name, health):
+#         self.health = health
+#         self.name = name
+#
+#     def hit(self):
+#         self.health -= 20
+#
+#     def live(self):
+#         if self.health > 0:
+#             print('Жив!\n')
+#         else:
+#             print('Погиб воин', self.name)
+#
+#     def true_false(self):
+#         if self.health > 0:
+#             return True
+#         else:
+#             return False
+#
+#
+# warrior_1 = Warrior('Вася', 100)
+# warrior_2 = Warrior('Петя', 100)
+# while True:
+#     answer = random.randint(1, 2)
+#     if answer == 1:
+#         warrior_2.hit()
+#         print('Атаковал 1 воин')
+#         print('Здоровье 2 воина:', warrior_2.health)
+#         warrior_2.live()
+#     elif answer == 2:
+#         warrior_1.hit()
+#         print('Атаковал 2 воин')
+#         print('Здоровье 1 воина:', warrior_1.health)
+#         warrior_1.live()
+#
+#     if not warrior_1.true_false() or not warrior_2.true_false():
+#         break
