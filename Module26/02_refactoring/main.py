274f27e2f7626ce0a24407d40716e19987f98816
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-17 02:17:05 +0400

    .

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -1,17 +1,15 @@
 list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
-to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
 
-# TODO провести рефакторинг кода
+def square():
+    for x in list_1:
+        for y in list_2:
+            yield x, y, x * y
+            if x * y == 56:
+                print('Found!!!')
+                return
+
+
+for elem in square():
+    print(*elem)
