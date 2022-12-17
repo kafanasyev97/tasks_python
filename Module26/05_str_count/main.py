23e79b7e0aca6a0f7ebca5a7ae03ecf1efdce55d
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-17 19:56:12 +0400

    .

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def strings_count(directory: str) -> Iterable[tuple]:
+    for address, dirs, files in os.walk(directory):
+        for file in files:
+            count = 0
+            if os.path.join(address, file).endswith('.py'):
+                curr_file = open(os.path.join(address, file), 'r', encoding='utf-8')
+                for line in curr_file.readlines():
+                    if line != '\n' and not line.startswith('#'):
+                        count += 1
+                yield os.path.join(address, file), count
+
+
+total_str = 0
+for element in strings_count(directory='..'):
+    print('Файл "{}": строк кода - {}'.format(element[0], element[1]))
+    total_str += element[1]
+print('Общее кол-во строк кода:', total_str)
+
