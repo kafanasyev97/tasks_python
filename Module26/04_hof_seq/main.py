6f902cfe45491d35381c8d578d82564783b669cd
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-17 18:56:25 +0400

    .

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def QHofstadter(s: list):
+    a = s[:]
+    count = 0
+    if a != [1, 1]:
+        return
+    while count != 10:
+        count += 1
+        q = a[-a[-1]] + a[-a[-2]]
+        a.append(q)
+        yield q
+
+
+user = QHofstadter([1, 1])
+for x in user:
+    print(x, end=' ')
