d4485ff6d2d6a82ebcc933e54d05a2cf9c55b3ab
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-21 18:23:37 +0400

    .

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def how_are_you(func: Callable) -> Callable:
+    """Декоратор. Спрашивает у пользователя как его дела."""
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> None:
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        func(*args, **kwargs)
+    return wrapped_func
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
