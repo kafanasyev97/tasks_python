a00e7d806f64f90aeff79011fa14a736818e9c69
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-22 00:56:46 +0400

    .

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def counter(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        wrapped_func.count += 1
+        res = func(*args, **kwargs)
+        print(f'Функция {func.__name__} была вызвана {wrapped_func.count} раз')
+        return res
+    wrapped_func.count = 0
+    return wrapped_func
+
+
+@counter
+def test():
+    print('test')
+
+
+test()
+test()
