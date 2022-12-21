6a421cf2f92bcba603c32f602eb858ef75f91b3c
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-22 00:08:54 +0400

    .

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def debug(func):
+    def wrapped_func(*args, **kwargs):
+        print('Вызывается', func.__name__, repr(args), repr(kwargs))
+        print(func.__name__, 'вернула значение', repr(func(*args, **kwargs)))
+        print(func(*args, **kwargs))
+        print()
+    return wrapped_func
+
+
+@debug
+def greeting(name, age=None):
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растёшь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+
+greeting("Том")
+greeting("Миша", age=100)
+greeting(name="Катя", age=16)
