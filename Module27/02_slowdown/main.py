bd0d9c489e15dccecb8881221da20e58ee39802a
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-21 18:43:34 +0400

    .

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+import time
+import functools
+from typing import Callable, Any
+
+
+def decor(func: Callable) -> Callable:
+    """Декоратор, замедляющий работу функции"""
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Any:
+        print('</----------\>')
+        time.sleep(3)
+        func(*args, **kwargs)
+        print('<\______/>')
+    return wrapped_func
+
+
+@decor
+def help_func() -> None:
+    print('помидоры')
+    print('--ветчина--')
+    print('~салат~')
+
+
+help_func()
