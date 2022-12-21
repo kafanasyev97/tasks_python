4f6a1866e2ade7580d70abc22500b1ff0f654206
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-21 19:25:44 +0400

    .

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+import datetime
+
+
+def logging(func):
+    def wrapped_func(*args, **kwargs):
+        print(func.__name__)
+        print(func.__doc__)
+        try:
+            res = func(*args, **kwargs)
+            return res
+        except Exception as exc:
+            with open('function_errors.log', 'a', encoding='utf-8') as file:
+                file.write(f'{datetime.datetime.now()},  {func.__name__},  {exc}\n')
+            print('Обнаружена ошибка!')
+    return wrapped_func
+
+
+@logging
+def help_func():
+    """Вспомогательная функция"""
+    result = 3 / 0
+    return result
+
+
+help_func()
