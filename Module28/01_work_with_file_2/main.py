8dc3a9a8e231bd4726419298e8881a25ce41f8d0
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-26 05:51:39 +0400

    .

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+class File:
+    """Контекст - менеджер для открытия и закрытия файлов"""
+    def __init__(self, name: str, mode: str) -> None:
+        self.name = name
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.name, self.mode, encoding='utf-8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.file.close()
+        return True
+
+
+with File('example.txt', 'w+') as file:
+    file.write('Всем привет!')
