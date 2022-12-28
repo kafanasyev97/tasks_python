34b10c05c11a92006be0947712e2623452d01a35
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-28 14:57:58 +0400

    .

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+def check_permission(text):
+    def need_func(func):
+        def wrapped_func(*args, **kwargs):
+            try:
+                if text in user_permissions:
+                    func(*args, **kwargs)
+                else:
+                    raise PermissionError(f'У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
+            except PermissionError as exc:
+                print(exc)
+        return wrapped_func
+    return need_func
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
