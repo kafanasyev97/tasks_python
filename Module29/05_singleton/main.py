a7a525d555f278a04ef0422f2a2561fc7e529406
Kirill Afanasyev <kirillmas222@gmail.com>
2022-12-29 20:43:42 +0400

    .

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        if not wrapper.instance:
+            wrapper.instance = cls(*args, **kwargs)
+        return wrapper.instance
+
+    wrapper.instance = None
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+
+print(my_obj is my_another_obj)
