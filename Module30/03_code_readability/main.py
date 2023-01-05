c93c91f5cc0f801c64452e6555742e414fc316f1
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-06 01:26:29 +0400

    .

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -6,8 +6,8 @@ def is_prime(number):
             return False
     return True
 
+if __name__ == '__main__':
+    result = [num for num in range(0, 1001) if is_prime(num)]
+    print(result)
 
-result = [num for num in range(0, 1001) if is_prime(num)]
-print(result)
-
-print(list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), range(2, 1001))))
+    print(list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), range(2, 1001))))
