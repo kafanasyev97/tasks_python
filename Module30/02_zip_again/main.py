2df390408ac3d86867efd83e97bfd74077a9bca9
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-06 01:25:57 +0400

    .

--- Module30/02_zip_again/main.py
+++ Module30/02_zip_again/main.py
@@ -1,8 +1,8 @@
 from typing import List
 
+if __name__ == '__main__':
+    letters: List[str] = ['a', 'b', 'c', 'd', 'e']
+    numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 
-letters: List[str] = ['a', 'b', 'c', 'd', 'e']
-numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
-
-results = list(map(lambda x, y: (x, y), letters, numbers))
-print(results)
+    results = list(map(lambda x, y: (x, y), letters, numbers))
+    print(results)
