e3ffc6f6209e3da9c33c7fec8128b22322e4867f
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-06 01:59:43 +0400

    .

---
+++ Module30/04_palindrome_the_return/main.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+def can_be_poly(val: str) -> bool:
+    return len(list(filter(lambda x: x % 2, Counter(val).values()))) <= 2
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
