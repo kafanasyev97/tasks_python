43c67b08e468343fcbd69e69edb181b56ddb4d14
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-11 03:18:14 +0400

    .

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+
+text = ['9999999999', '999999-999', '99999x9999']
+count = 1
+
+for x in text:
+    res = re.findall('[89]\d{9}', x)
+    if res:
+        print(count, 'номер: всё в порядке')
+    else:
+        print(count, 'номер: не подходит')
+    count += 1
