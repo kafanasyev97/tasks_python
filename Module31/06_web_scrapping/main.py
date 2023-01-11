c95d982db854cbbe5ff4fc08fd567d2bf31f368a
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-11 10:19:55 +0400

    .

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import requests
+from re import findall
+
+response = requests.get('http://www.columbia.edu/~fdc/sample.html')
+# Для любого сайта, я так полагаю, необходимо просто вместо ссылки поставить input('Вставьте ссылку: ')
+result_beta = findall(r'>.+</h3>', response.text)
+release = list(map(lambda x: x[1:-5], result_beta))
+print(release)
