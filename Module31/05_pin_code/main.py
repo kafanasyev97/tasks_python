d944a74162cbb995fc701ea1aac46d0b45b10f28
Kirill Afanasyev <kirillmas222@gmail.com>
2023-01-11 09:53:28 +0400

    .

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -3,7 +3,7 @@ import itertools
 
 # for x in itertools.permutations('0123456789', 4):
 #     print(*x)
-#
+
 
 pin_code = itertools.product('0123456789', repeat=4)
 print('Результат работы программы:')
